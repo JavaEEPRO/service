7adab24af03d097d367f5da25a2abc981593459f
JavaEEPRO <j2eengineer@gmail.com>
2020-07-11 18:45:03 +0300

    added TestTaskConfig

---
+++ src/test/java/si/inspired/spring/TestTaskConfig.java
@@ -0,0 +1,10 @@
+package si.inspired.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "si.inspired.task" })
+public class TestTaskConfig {
+
+}
