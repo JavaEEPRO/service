09d96da3012d4ed6fdf16e91795f3f2a78f1840a
JavaEEPRO <j2eengineer@gmail.com>
2020-07-11 17:27:44 +0300

    added SpringTaskConfig

---
+++ src/main/java/si/inspired/spring/SpringTaskConfig.java
@@ -0,0 +1,12 @@
+package si.inspired.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ComponentScan({ "si.inspired.task" })
+public class SpringTaskConfig {
+
+}
